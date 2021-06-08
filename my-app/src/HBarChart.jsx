66933f893fa6f0a4b314b68ef015a1cc4d75c875
= <colizu2020@gmail.com>
2021-06-07 20:31:04 -0700

    commit

--- my-app/src/HBarChart.jsx
+++ my-app/src/HBarChart.jsx
@@ -31,22 +31,11 @@ const data = [
     uv: 1890,
     pv: 4800,
     amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  }
+  
 ];
 
-export default class HBarChart extends PureComponent {
+export default class Example extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/simple-bar-chart-tpz8r';
 
   render() {
@@ -54,7 +43,7 @@ export default class HBarChart extends PureComponent {
       <ResponsiveContainer width="100%" height="100%">
         <BarChart
           width={500}
-          height={300}
+          height={200}
           data={data}
           margin={{
             top: 5,
@@ -66,8 +55,6 @@ export default class HBarChart extends PureComponent {
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
-          <Tooltip />
-          <Legend />
           <Bar dataKey="pv" fill="#8884d8" />
           <Bar dataKey="uv" fill="#82ca9d" />
         </BarChart>
