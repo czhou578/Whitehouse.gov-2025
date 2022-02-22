d457c214193216154aa53eff52d2a24b69322a99
czhou578 <colizu2020@gmail.com>
2022-02-22 11:20:16 -0800

    styled form

--- my-app/src/Elections.jsx
+++ my-app/src/Elections.jsx
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { Card, Icon, Container, Form } from "semantic-ui-react";
+import { Card, Icon, Form } from "semantic-ui-react";
 import "./elections.css";
 
 const Elections = () => {
   const [elections, setElections] = useState([]);
+  const [address, setAddress] = useState("")
+  const [city, setCity] = useState("")
+  const [stateInitial, setStateInitial] = useState("")
+  const [zipCode, setZipCode] = useState("")
 
-  useEffect(async () => {
-    let data = await fetch(
-      "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyA9Ffz_6qbDL4H4o8peICHb1MsG3r0YbZk"
-    );
 
-    let elections = await data.json();
-    console.log(elections);
+  // useEffect(() => {
+  //   async function fetchElection () {
+  //     let data = await fetch(
+  //       "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyA9Ffz_6qbDL4H4o8peICHb1MsG3r0YbZk"
+  //     );
+    
+  //     let elections = await data.json();
+    
+  //     setElections(elections.elections);
+  //   }
 
-    setElections(elections.elections);
-  }, []);
+  //   fetchElection()
+  // }, []);
+
+  const sendRepData = () => {
+    
+  }
 
   return (
     <div className="electionWrap">
@@ -31,19 +43,21 @@ const Elections = () => {
           );
         })}
       </Card.Group>
-      <div>
+      <div className="inputGroup">
+        <h2>Find Your Local Representative</h2>
         <Form>
           <Form.Group widths="equal">
-            <Form.Input fluid label="First name" placeholder="First name" />
-            <Form.Input fluid label="Last name" placeholder="Last name" />
+            <Form.Input fluid label="Address" placeholder="Address" onChange={(e) => setAddress(e.target.value)}/>
+            <Form.Input fluid label="City" placeholder="City"  onChange={(e) => setCity(e.target.value)}/>
             <Form.Input
               fluid
               label="State Initial"
               placeholder="State Initial"
+              onChange={(e) => setStateInitial(e.target.value)}
             />
           </Form.Group>
           <Form.Group>
-            <Form.Input fluid label="ZIP Code" placeholder="ZIP Code" />
+            <Form.Input fluid label="ZIP Code" placeholder="ZIP Code" className="zip" onChange={(e) => setZipCode(e.target.value)}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
