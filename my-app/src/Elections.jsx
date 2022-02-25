d46dd529a963e080efd3c559415a1abfef84a8d4
czhou578 <colizu2020@gmail.com>
2022-02-25 15:32:09 -0800

    fetched data from remote api

--- my-app/src/Elections.jsx
+++ my-app/src/Elections.jsx
@@ -4,29 +4,53 @@ import "./elections.css";
 
 const Elections = () => {
   const [elections, setElections] = useState([]);
-  const [address, setAddress] = useState("")
-  const [city, setCity] = useState("")
-  const [stateInitial, setStateInitial] = useState("")
-  const [zipCode, setZipCode] = useState("")
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [stateInitial, setStateInitial] = useState("");
+  const [repData, setRepData] = useState();
 
+  useEffect(() => {
+    async function fetchElection() {
+      let data = await fetch(
+        "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyA9Ffz_6qbDL4H4o8peICHb1MsG3r0YbZk"
+      );
 
-  // useEffect(() => {
-  //   async function fetchElection () {
-  //     let data = await fetch(
-  //       "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyA9Ffz_6qbDL4H4o8peICHb1MsG3r0YbZk"
-  //     );
-    
-  //     let elections = await data.json();
-    
-  //     setElections(elections.elections);
-  //   }
+      let elections = await data.json();
 
-  //   fetchElection()
-  // }, []);
+      setElections(elections.elections);
+    }
 
-  const sendRepData = () => {
-    
-  }
+    fetchElection();
+  }, [elections.length === 0]);
+
+  const sendRepData = async () => {
+    let resultString = [];
+    let urlSubString = ``;
+    let addressSplitString = address.split(" ");
+    let citySplitString = city.split(" ");
+
+    resultString.push(...addressSplitString, ...citySplitString, stateInitial);
+
+    for (let i = 0; i < resultString.length; i++) {
+      if (i === resultString.length - 1) {
+        urlSubString += `${resultString[i]}`;
+        break;
+      }
+
+      urlSubString += `${resultString[i]}%20`;
+    }
+
+    // console.log(urlSubString);
+
+    let url = `https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyA9Ffz_6qbDL4H4o8peICHb1MsG3r0YbZk&address=${urlSubString}`;
+
+    const result = await fetch(url);
+    const answer = await result.json();
+
+    setRepData([...answer.officials[2], ...answer.officials[3]]);
+
+    console.log(answer);
+  };
 
   return (
     <div className="electionWrap">
@@ -47,8 +71,18 @@ const Elections = () => {
         <h2>Find Your Local Representative</h2>
         <Form>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Address" placeholder="Address" onChange={(e) => setAddress(e.target.value)}/>
-            <Form.Input fluid label="City" placeholder="City"  onChange={(e) => setCity(e.target.value)}/>
+            <Form.Input
+              fluid
+              label="Address"
+              placeholder="Address"
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <Form.Input
+              fluid
+              label="City"
+              placeholder="City"
+              onChange={(e) => setCity(e.target.value)}
+            />
             <Form.Input
               fluid
               label="State Initial"
@@ -56,10 +90,7 @@ const Elections = () => {
               onChange={(e) => setStateInitial(e.target.value)}
             />
           </Form.Group>
-          <Form.Group>
-            <Form.Input fluid label="ZIP Code" placeholder="ZIP Code" className="zip" onChange={(e) => setZipCode(e.target.value)}/>
-          </Form.Group>
-          <Form.Button>Submit</Form.Button>
+          <Form.Button onClick={() => sendRepData()}>Submit</Form.Button>
         </Form>
       </div>
     </div>
